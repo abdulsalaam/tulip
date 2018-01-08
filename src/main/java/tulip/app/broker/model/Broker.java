6596d77e641c079564183909705fcaf89f021d43
yanisguimard <yanisguim@gmail.com>
2018-01-08 17:58:56 +0100

    Implement Broker methods

--- src/main/java/tulip/app/broker/model/Broker.java
+++ src/main/java/tulip/app/broker/model/Broker.java
@@ -1,17 +1,23 @@
 package tulip.app.broker.model;
 
+import tulip.app.appMessage.ActorType;
+import tulip.app.appMessage.AppMessage;
+import tulip.app.appMessage.AppMessageContentType;
+import tulip.app.client.model.Client;
 import tulip.app.order.Order;
-import tulip.app.order.PurchaseOrder;
-import tulip.app.order.SellOrder;
+import tulip.app.order.OrderType;
 import tulip.service.producerConsumer.Consumer;
 import tulip.service.producerConsumer.Producer;
+import tulip.service.producerConsumer.ProducerMessenger;
 import tulip.service.producerConsumer.messages.ContentType;
 import tulip.service.producerConsumer.messages.Message;
 import tulip.service.producerConsumer.messages.Target;
 
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 
-public class Broker {
+public class Broker implements ProducerMessenger {
 
     /** Name of the broker */
     private String name;
@@ -22,17 +28,25 @@ public class Broker {
     /** Commission rate applied by the broker on each transaction */
     private double commissionRate;
     /** Clients list of the broker */
-    private List<String> clients;
+    private List<String> clients = new ArrayList<>();
     /** Orders (which can be both purchases and sellings) not yet proceeded */
-    private List<Order> pendingOrders;
+    private List<Order> pendingOrders = new ArrayList<>();
     /** Counts the selling orders proceeded by the broker */
     private int sellOrderCounter;
     /** Counts the purchase orders proceeded by the broker */
     private int purchaseOrderCounter;
-    /** Producer role for the broker (with Stock Exchange) */
-    Producer brokerProducer = new Producer(name);
-    /** Consumer role for the broker (with Client) */
-    Consumer brokerConsumer = new Consumer(name);
+
+    private Map<String, Double> marketState = new HashMap<>();
+
+    private List<String> clientsRequestingRegistration = new ArrayList<>();
+
+    private List<String> clientsRegistered = new ArrayList<>();
+
+    ServerSocket serverSocket;
+    Consumer brokerConsumer = new Consumer("broker", serverSocket);
+    Socket socket;
+    Producer brokerProducer = new Producer("broker", socket, this);
+
 
     /**
      * Constructor
@@ -51,14 +65,24 @@ public class Broker {
      * Sends registering request to stock exchange
      */
     public void registerToStockExchange() {
-        brokerProducer.produce(new Message(Target.consumer, ContentType.app, "registering request"));
-        this.isRegistered = true;
+        if (brokerProducer.canProduce()) {
+            brokerProducer.produce(new AppMessage(
+                    this.name, ActorType.broker, "stockExchange", ActorType.stockExchange, AppMessageContentType.registrationRequest, this.name
+            ));
+        }
     }
 
     /**
      * Registers a client
      */
-    public void registerClient(){
+    public void registerClient(String clientName){
+        //String clientToRegister = clientsRequestingRegistration.get(0);
+
+        if(brokerProducer.canProduce()) {
+            brokerProducer.produce(new AppMessage(
+                    this.name, ActorType.broker, clientName, ActorType.client, AppMessageContentType.registrationAcknowledgment, ""
+            ));
+        }
     }
 
     /**
@@ -66,13 +90,23 @@ public class Broker {
      * market state information
      */
     public void requestMarketState(){
-        brokerProducer.produce(new Message(Target.consumer, ContentType.app, "registering request"));    }
+        if(brokerProducer.canProduce()){
+            brokerProducer.produce(new AppMessage(
+                    this.name, ActorType.broker, "stockExchange", ActorType.stockExchange, AppMessageContentType.marketStateRequest, ""
+            ));
+        }
+    }
 
     /**
-     * Checks whether a client is registered or notf
+     * Checks whether a client is registered or not
      * @return true if the client is registered, false otherwise
      */
-    public boolean checkClientRegistered(){
+    public boolean checkClientRegistered(String clientName){
+        for(String s : clientsRegistered) {
+            if(s.equals(clientName)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -84,7 +118,7 @@ public class Broker {
      */
     public void placeSellOrder(String company, String client, int nbOfStocks, double minSellingPrice){
         pendingOrders.add(
-                new SellOrder(++sellOrderCounter,company, client, name, new Date(), nbOfStocks, minSellingPrice)
+                new Order(++sellOrderCounter, OrderType.purchase, company, client, name, minSellingPrice, nbOfStocks)
         );
     }
 
@@ -96,52 +130,89 @@ public class Broker {
      */
     public void placePurchaseOrder(String company, String client, int nbOfStocks, double maxPurchasingPrice) {
         pendingOrders.add(
-                new PurchaseOrder(++purchaseOrderCounter, company, client, name, new Date(), nbOfStocks, maxPurchasingPrice)
+                new Order(++purchaseOrderCounter, OrderType.purchase, company, client, name, maxPurchasingPrice, nbOfStocks)
         );
     }
-
-    /**
-     * To delete ?
-     */
-    public void notifyOfTransaction(){}
 
     /**
      * After an agreement has been made, notifies the client that the order
      * has been proceeded.
      */
-    public void notifyClientOfTransaction(){
-        brokerProducer.produce(new Message(Target.producer, ContentType.app, "Notification"));
+    public void notifyOfTransaction(String clientName){
+        if(brokerProducer.canProduce()){
+            brokerProducer.produce(new AppMessage(
+                    this.name, ActorType.broker, clientName, ActorType.client, AppMessageContentType.purchaseOrder, ""
+
+            ));
+        }
     }
+
 
     // Improvement : put getActualAmount in Order class
     /**
-     * After proceeding a Purchase order, the broker calculates its commission and updates
+     * After proceeding an order, the broker calculates its commission and updates
      * his cach accordingly
      * @param order the order from which the commission is calculated
      */
-    public void calculateCommission(PurchaseOrder order){
-        //double commission = order.getActualAmount() * commissionRate;
-        //this.cash += commission;
+    public void calculateCommission(Order order){
+        double commission = order.getActualAmount() * commissionRate;
+        this.cash += commission;
     }
-    /**
-     * After proceeding a Sell order, the broker calculates its commission and updates
-     * his cach accordingly
-     * @param order the order from which the commission is calculated
-     */
-    public void calculateCommission(SellOrder order){
-        //double commission = order.getActualAmount() * commissionRate;
-        //this.cash += commission;
-    }
+
 
     /**
      * Sends to the client the updated stock values
      */
-    public void notifyClientsOfPriceChanges(){}
+    public void notifyClientsOfPriceChanges(){
+
+    }
 
     /**
      * When all his clients close the day, the closer closes it as well
      * and informs the stock exchange
      */
-    public void closeTheDay(){}
+    public void closeTheDay(){
 
+    }
+
+    @Override
+    public void uponReceiptOfAppMessage(AppMessage message) {
+        switch (message.getAppMessageContentType()){
+
+            case sellOrder:
+                // Add to pending orders
+                break;
+
+            case purchaseOrder:
+                // Add to pending orders
+                break;
+
+            case marketStateReply:
+                // Add all elements of the map to this map
+                break;
+
+            case marketStateRequest:
+                if (brokerProducer.canProduce()) {
+                    brokerProducer.produce(new AppMessage(
+                            this.name, ActorType.broker, message.getSender(), ActorType.client, AppMessageContentType.marketStateReply, marketState.toString()
+
+                    ));
+                }
+                break;
+
+            case registrationAcknowledgment:
+                this.isRegistered = true;
+                break;
+
+            case registrationRequest:
+                registerClient(message.getSender());
+                break;
+
+            case endOfDayNotification:
+                // Add to list
+
+
+
+        }
+    }
 }
